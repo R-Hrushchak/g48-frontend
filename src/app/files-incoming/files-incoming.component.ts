63ece7930a4b2a226843ad666ed38016a2e56ed4
Roman Hrushchak <roman.hrushchak.ua@gmail.com>
2018-04-22 18:07:46 +0300

    mock

--- src/app/files-incoming/files-incoming.component.ts
+++ src/app/files-incoming/files-incoming.component.ts
@@ -19,41 +19,49 @@ export class FilesIncomingComponent implements OnInit {
   private handleData(file) {
     let parsedJSON = JSON.parse(file.attributes.message);
     parsedJSON['message_decrypted'] = 'Information to save';
-
-    parsedJSON['message'] = parsedJSON['message'].substr(1, parsedJSON['message'].length - 2);
-
-    // decrypt message
-
-    const fileService = this.fileService;
-    const userService = this.userService;
-    const cryptoService = this.cryptoService;
-
-    const privateKeyJSON = localStorage.getItem('private_key');
-
-    crypto.subtle.importKey('jwk',
-      JSON.parse(privateKeyJSON),
-      {
-        name: 'RSA-OAEP',
-        hash: {name: 'SHA-256'},
-      },
-      false,
-      ['decrypt']
-    ).then(function (privateKey) {
-      crypto.subtle.decrypt({
-          name: 'RSA-OAEP',
-        },
-        privateKey,
-        cryptoService.str2ab(parsedJSON['message'])
-      )
-        .then(function (result) {
-          console.log(cryptoService.ab2str(result));
-          parsedJSON['message_decrypted'] = result;
-        });
-    });
-
+    parsedJSON['message'] = '⽠᜷鋔崷ⱽ뫜〺襐⮆堳엕⍖♔刃팒�繖ត漄禲✥纭땷䢿఻較㧣循윱卝ﭔ鞴㱷褍吏佀쀵悛拇錅繰삌⒗�䡠䌷�ꆼ릲⬢�釒牗䳓䖊Ꝗ홪ᆼ찗懶പ㐕Ǐ᫱訒㨛귤鍴벬暭縰ࣤ�靔譪褂秹趴鷴ꗰើⰳ촕醙௧킯埧藕윊辁ు鶬㉍꯺�ή苪僱꺗뙿燊깭裘䐆툗ᴥ鿬㚟⅏귶孱璉Ⴑᨉ䥸ﴵ특ﴉ';
     return parsedJSON;
   }
 
+  /*
+    private handleData(file) {
+      let parsedJSON = JSON.parse(file.attributes.message);
+      parsedJSON['message_decrypted'] = 'Information to save';
+
+      parsedJSON['message'] = parsedJSON['message'].substr(1, parsedJSON['message'].length - 2);
+
+      // decrypt message
+
+      const fileService = this.fileService;
+      const userService = this.userService;
+      const cryptoService = this.cryptoService;
+
+      const privateKeyJSON = localStorage.getItem('private_key');
+
+      crypto.subtle.importKey('jwk',
+        JSON.parse(privateKeyJSON),
+        {
+          name: 'RSA-OAEP',
+          hash: {name: 'SHA-256'},
+        },
+        false,
+        ['decrypt']
+      ).then(function (privateKey) {
+        crypto.subtle.decrypt({
+            name: 'RSA-OAEP',
+          },
+          privateKey,
+          cryptoService.str2ab(parsedJSON['message'])
+        )
+          .then(function (result) {
+            console.log(cryptoService.ab2str(result));
+            parsedJSON['message_decrypted'] = result;
+          });
+      });
+
+      return parsedJSON;
+    }
+*/
   setData(data: any[]) {
     this.files = data['data'];
 
@@ -69,7 +77,6 @@ export class FilesIncomingComponent implements OnInit {
     const cryptoService = this.cryptoService;
 
     this.fileService.getFiles().then(result => this.setData(result));
-    ;
   }
 
   ngOnInit() {
